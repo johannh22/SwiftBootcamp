d632a07e7fa87335b638c21d28434fcb9f9eba0c
johannh22 <johannhomonnai@gmail.com>
2021-06-16 17:34:23 -0300

    Protocols and Delegates example

---
+++ Protocols and Delegates/Protocols and Delegates/main.swift
@@ -0,0 +1,27 @@
+
+protocol AdvancedLifeSupport {
+    func performCPR()
+}
+
+class EmergencyCallHandler {
+    var delegate: AdvancedLifeSupport?
+    
+    func assessSituation() {
+        print("Can you tell me what happened?")
+    }
+    
+    func medicalEmergency() {
+        delegate?.performCPR()
+    }
+}
+
+struct Paramedic: AdvancedLifeSupport {
+    
+    init(handler: EmergencyCallHandler) {
+        handler.delegate = self
+    }
+    
+    func performCPR(){
+        print("The paramedic does chest compressions, 30 per second.")
+    }
+}
